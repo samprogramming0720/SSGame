29190480f2734cb704f8047f603c66588c300265
Seung Bin Lee <seriskale0720@gmail.com>
2017-07-20 18:42:33 -0400

    first commit

---
+++ html/src/com/seungbin/ssgame/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.seungbin.ssgame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.seungbin.ssgame.SSGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new SSGame();
+        }
+}
